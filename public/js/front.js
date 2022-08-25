196fc4f8b295e5b0b57f1a60edef6c20e6d68bb5
Tommaso <tommaso.lodesani@gmail.com>
2022-08-25 16:49:40 +0200

    setting tommaso

--- public/js/front.js
+++ public/js/front.js
@@ -40227,8 +40227,8 @@ var app = new Vue({
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\Utente\Desktop\giochi laravel\resources\js\front.js */"./resources/js/front.js");
-module.exports = __webpack_require__(/*! C:\Users\Utente\Desktop\giochi laravel\resources\sass\back.scss */"./resources/sass/back.scss");
+__webpack_require__(/*! C:\Users\Tommaso\Desktop\BOOLEAN\ESERCIZI\laravel-games\resources\js\front.js */"./resources/js/front.js");
+module.exports = __webpack_require__(/*! C:\Users\Tommaso\Desktop\BOOLEAN\ESERCIZI\laravel-games\resources\sass\back.scss */"./resources/sass/back.scss");
 
 
 /***/ })
